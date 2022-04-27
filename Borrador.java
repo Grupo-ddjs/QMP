734b37e1a2640b404c11bbaddc481f135580fd99
Santiago <80548565+santysisi@users.noreply.github.com>
2022-04-27 19:55:23 -0300

    Se sube clase Borrador sin miedo al exito

---
+++ Borrador.java
@@ -0,0 +1,11 @@
+class Borrador
+    // ... demás atributos y métodos
+
+    public void especificarTipo(TipoPrenda){
+	this.prenda = tipoPrenda;
+    }
+    public Prenda crearPrenda(){
+	 return new Prenda(this.tipo,this.material,...);
+    }
+
+}
