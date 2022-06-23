eabb31d2248acce38eaf26835bdd94b3ad6f6142
Tomas <tsuarez@frba.utn.edu.ar>
2022-06-22 23:49:14 -0300

    Mas de iteracion 6

--- Usuario.java
+++ Usuario.java
@@ -2,9 +2,23 @@ class Usuario {
 
 	List<Guardarropa> guardarropas;
 	List<Atuendos> sugerencias;
+	String mail;
+	NotificationService notificationService;
+	MailSenderService mailSender;
 	
 	void recibirSugerencia(Atuendo atuendo){
 		sugerencias.add(atuendo);
+	}
+	
+	void notificar(String clima) {
+		if (clima == "STORM")
+			notificationService.notify("Llueve de arriba para abajo");
+		else if (clima == "HAIL")
+			notificationService.notify("Guarda el auto que caen piedras");
+	}
+	
+	void enviarMail(String clima) {
+		mailSender.send(address,message)
 	}
 
 }
